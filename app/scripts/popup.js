6a05615ecf4d110dd28f1bbc24e9786a1c3adcd2
Jimmy Liu <sicong.liu98@gmail.com>
2015-03-17 23:16:49 -0700

    Added Angular.js in, created basic routes and modified HTML to include Angular

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,20 @@
 'use strict';
 
 console.log('\'Allo \'Allo! Popup');
+
+var eventBriteApp = angular.module('eventBrite', ['ngRoute']);
+
+eventBriteApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            });
+    });
+
+eventBriteApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'This is me testing routing!';
+});

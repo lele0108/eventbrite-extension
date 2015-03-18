022de437974ae1ea6ea6d88c71957fdbabbc5ca6
Jimmy Liu <sicong.liu98@gmail.com>
2015-03-17 23:36:19 -0700

    Additional routing and setup of seperate views for Search and Search results

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -9,12 +9,27 @@ eventBriteApp.config(function($routeProvider) {
 
             // route for the home page
             .when('/', {
-                templateUrl : 'pages/home.html',
-                controller  : 'mainController'
+                templateUrl : 'views/home.html',
+                controller  : 'homeController'
+            })
+
+            .when('/search', {
+                templateUrl : 'views/search.html',
+                controller  : 'searchController'
             });
     });
 
 eventBriteApp.controller('mainController', function($scope) {
         // create a message to display in our view
         $scope.message = 'This is me testing routing!';
+});
+
+eventBriteApp.controller('homeController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Search Bar would go here';
+});
+
+eventBriteApp.controller('searchController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Search results are now being displayed';
 });

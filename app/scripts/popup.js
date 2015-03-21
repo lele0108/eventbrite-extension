e4f0c86bf7c99f78a413cf9ef9ec522b34068693
Jimmy Liu <sicong.liu98@gmail.com>
2015-03-20 22:09:33 -0700

    basic EventBrite api calls and two way data-binding

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -38,16 +38,52 @@ eventBriteApp.controller('homeController', function($scope, $location) {
         };
 });
 
-eventBriteApp.controller('searchController', function($scope, $routeParams) {
+//Angular service to save Event information between controllers for better resource and performance
+eventBriteApp.service('dataService', function () {
+        var property = 'First';
+
+        return {
+            getProperty: function () {
+                return property;
+            },
+            setProperty: function(value) {
+                property = value;
+            }
+        };
+    });
+
+eventBriteApp.controller('searchController', function($scope, $routeParams, $http, dataService) {
         // create a message to display in our view
         $scope.message = 'Search results are now being displayed';
+        $scope.events = {}; //variable for storing events
+        //GET request for EventBrite
+        var config = {
+            q:'hackathon', 
+            "location.address":'San Francisco', 
+            "location.within":"15mi", 
+            "start_date.range_start":'2015-03-20T21:18:02Z', 
+            "start_date.range_end":'2015-03-31T21:18:07Z', 
+            token:'IOTP7KEXPCDAJTKKPTJB',
+        };
+        $http.get('https://www.eventbriteapi.com/v3/events/search/', {params: config}).
+          success(function(data, status, headers, config) {
+            $scope.events = data.events;
+            console.log($scope.events);
+          }).
+          error(function(data, status, headers, config) {
+            console.log(data);
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+        $scope.alert = function(index){
+            dataService.setProperty($scope.events[index]);
+        };
 });
 
-eventBriteApp.controller('detailsController', function($scope, $routeParams) {
+eventBriteApp.controller('detailsController', function($scope, $routeParams, dataService) {
         // create a message to display in our view
         $scope.message = 'More details about the event displayed here';
+        //Getting the event details from the Angular service saved from searchController
+        $scope.event = dataService.getProperty();
+        console.log($scope.event);
 });
-
-function event() {
-
-}
